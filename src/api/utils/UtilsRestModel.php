e4abd6634e62156b9e52941928c240696eab203e
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-21 09:02:40 -0300

    Add files via upload

---
+++ src/api/utils/UtilsRestModel.php
@@ -0,0 +1,23 @@
+<?php
+
+class UtilsRestModel{
+
+    public function xplParam($param){
+        
+        return explode(",", $param);
+
+    }
+
+    public function verifyString($param){
+
+        if(strpos($param, "SELECT") || strpos($param, "FROM") 
+        || strpos($param, "JOIN") || strpos($param, "WHERE")
+        || strpos($param, "DELETE") || strpos($param, "UPDATE")
+        || strpos($param, "ORDER") || strpos($param, "GROUP")
+        || strpos($param, "BY")){
+            return true;
+        }
+        return false;
+    }
+
+}
