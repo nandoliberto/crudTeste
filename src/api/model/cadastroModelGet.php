929b5a4f2407a9ff227e061aacb05c6d87e238b3
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-21 09:10:24 -0300

    Add files via upload

---
+++ src/api/model/cadastroModelGet.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once("../repository/restmodel.php");
+
+class CadastroModelGet extends Restmodel{
+
+    public function modelGetId($param){
+        
+        $this->select("prd_sku as SKU, prd_name as NAME, prd_name as PRICE, prd_name as QTD, prd_category as CATEGORY, prd_descricao as DESCRICAO");
+        $this->from("product");
+        $this->where(array("prd_id"=>$param));
+        $ret = $this->exec();
+        return $ret;
+    }
+
+    public function modelGetAll(){
+        $this->select("prd_id as ID, prd_sku as SKU, prd_name as NAME, prd_name as PRICE, prd_name as QTD, prd_category as CATEGORY, prd_descricao as DESCRICAO");
+        $this->from("product");
+        $ret = $this->exec();
+       
+        return $ret;
+    }
+
+}
---
+++ src/api/model/cadastroModelPost.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("../repository/restmodel.php");
+
+class CadastroModel extends Restmodel{
+
+    public function insertCad($dados){
+        
+        $this->insert("product");
+        $this->colummInsert("prd_sku, prd_name, prd_price, prd_qtd, prd_category, prd_descricao");
+        $this->valueInsert($dados->sku.",".$dados->productName.",".$dados->price.",".$dados->qtd.",".$dados->category.",".$dados->descricao);
+        $ret = $this->exec();
+        
+        return $ret;
+
+    }
+
+}
