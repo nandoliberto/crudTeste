81958f399df4bc9f19317c59bef94afe26178de5
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-21 09:01:00 -0300

    Create index.php

---
+++ src/api/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>APIS</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content=" width=devide-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>ATENCAO,ESTAS SAO APIS REST, PARA SABER COMO CONUMI-LAS ACESSAR O LINK</div>
+        <a href="../html/manual.html">Manual APIs</a>
+    </body>
+</html>
