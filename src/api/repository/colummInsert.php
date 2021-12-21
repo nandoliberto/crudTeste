538317c2b6d646fcc58b74402602b05bd562d845
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-21 09:11:13 -0300

    Create colummInsert.php

---
+++ src/api/repository/colummInsert.php
@@ -0,0 +1,39 @@
+<?php
+
+class ColummInsert {
+    public function _colummInsert($param){
+        try{
+
+            if(!strpos($param, ",")){
+
+                return $param.") values (";
+
+            }else{
+                
+                $xpl = explode(",", trim($param));
+
+                $arr;
+                
+                if(count($xpl) > 0){
+
+                    for ($i=0; $i < count($xpl); $i++) { 
+                    
+                        if(!isset($arr)){
+                            $arr = $xpl[$i];
+                            
+                        }else{
+                            $arr .= ",".$xpl[$i];
+                        }
+                    }
+                    
+                    return $arr .= ") values (";
+
+                }else{
+                    throw("Funçao ".__FUNCTION__.  " necessario pelo menos um parametro");
+                }
+            }
+        }catch (\Exception $e) {
+            throw("Erro :".__FUNCTION__." ". $e->getMessage());
+        }
+    }
+}
