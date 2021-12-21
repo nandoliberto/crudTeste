33b9a81fa747c4b343e7ccdb4d9f9441af11907b
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-21 09:04:18 -0300

    Create controllerPost.php

---
+++ src/api/controller/controllerPost.php
@@ -0,0 +1,21 @@
+<?php
+include_once("../model/cadastroModelPost.php");
+require_once("valDataPost/valDataPost.php");
+
+class ControllerPost extends CadastroModel{
+
+    public function gravaDados($param){
+
+        $valDatapost = new ValDataPost();
+        
+        $retVerifyData = $valDatapost->verifyData($param);
+        
+        if($retVerifyData){
+            return $retVerifyData;
+        }
+        
+        $ret = $this->insertCad($param);
+        
+        return $ret;
+    }
+}
