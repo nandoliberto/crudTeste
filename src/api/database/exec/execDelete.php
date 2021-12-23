2aaf9dcc4a4829d80585c7c78ba93869ea98bdef
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 11:53:32 -0300

    Create execDelete.php

---
+++ src/api/database/exec/execDelete.php
@@ -0,0 +1,15 @@
+<?php
+
+class DeleteDatabase{
+    public function deleteExec($pdo, $query){
+        // try {
+        //     $ret = $pdo->exec($query);
+
+        //     if($ret == 1){
+        //         return "registro excluido com sucesso";
+        //     }
+        // } catch (\Exception $e) {
+        //     throw("Erro :".__FUNCTION__." ". $e->getMessage());
+        // }
+    }
+}
