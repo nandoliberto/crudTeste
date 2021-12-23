e8aa73b92444e4d1cc8c077a6348364ca6048d48
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 18:15:02 -0300

    Add files via upload

---
+++ src/api/controller/valDataPut/valDataPut.php
@@ -0,0 +1,74 @@
+<?php
+
+class ValDataPut {
+
+    public function verifyData($dados){
+        
+        $i = 0;
+
+        if(!isset($dados_id) && !is_numeric($dados->id)){
+            $ret["retorno"][$i]["Erro"] = "O campo ID é obrigatorio e deve ser numerico.";
+        }
+
+        if(isset($dados->sku) && $dados->sku == ""){
+            $ret["retorno"][$i]["Erro"] = "O campo SKU e obrigatorio";
+            $i++;
+        }
+
+        if(isset($dados->sku) && $dados->sku != ""){
+            if(!preg_match("/[A-Za-z]/", $dados->sku)){
+                $ret["retorno"][$i]["Erro"] = "O campo SKU deve conter letras e numeros";
+                $i++;
+            }
+    
+            if(!preg_match("/[0-9]/", $dados->sku)){
+                $ret["retorno"][$i]["Erro"] = "O campo SKU deve conter letras e numeros";
+                $i++;
+            }
+        }
+
+    
+        if(isset($dados->productName) && $dados->productName == ""){
+            $ret["retorno"][$i]["Erro"] = "O campo nome do produto e obrigatorio";
+            $i++;
+        }
+
+        if(isset($dados->price) && !is_float($dados->price)){
+            $ret["retorno"][$i]["Erro"] = "O campo preco e obrigatorio e deve ser um float";
+            $i++;
+        }
+
+        if(isset($dados->qtd)){
+            $ret["retorno"][$i]["Erro"] = "O campo quantidade e obrigatorio";
+            $i++;
+
+            if(is_numeric($dados->qtd)){
+                $ret["retorno"][$i]["Erro"] = "O campo quantidade deve conter apenas numeros";
+                $i++;
+            }
+        }
+        
+        if(isset($dados->category)){
+           
+            if($dados->category != "Category 1" && $dados->category != "Category 2" &&
+            $dados->category != "Category 3" && $dados->category != "Category 4"){
+                $ret["retorno"][$i]["Erro"] = "O campo categoria deve ser  preenchido como Category 1, 2, 3 ou 4";
+                $i++;
+            }
+        }
+        
+        if(isset($dados->descricao) && $dados->descricao == ""){
+            $ret["retorno"][$i]["Erro"] = "O campo descricao e obrigatorio";
+            $i++;
+
+            if(strlen($dados->descricao > 250)){
+                $ret["retorno"][$i]["Erro"] = "O campo descricao deve conter no maximo 250 caracteres";
+                $i++;
+            }
+        }
+
+        if($ret["retorno"]){
+            return $ret["retorno"];
+        }
+    }
+}
