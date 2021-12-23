39b8bd8d6afecbe76968bb15b54f8818d98379f8
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 11:58:24 -0300

    Add files via upload

--- src/api/model/cadastroModelPost.php
+++ src/api/model/cadastroModelPost.php
@@ -9,6 +9,7 @@ class CadastroModel extends Restmodel{
         $this->insert("product");
         $this->colummInsert("prd_sku, prd_name, prd_price, prd_qtd, prd_category, prd_descricao");
         $this->valueInsert($dados->sku.",".$dados->productName.",".$dados->price.",".$dados->qtd.",".$dados->category.",".$dados->descricao);
+        
         $ret = $this->exec();
         
         return $ret;
