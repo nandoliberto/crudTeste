c365d36d6dbd2f49e7bf9a463c1f2b3fe0b7545f
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 18:18:28 -0300

    Add files via upload

--- src/api/database/queryExec.php
+++ src/api/database/queryExec.php
@@ -5,6 +5,7 @@ require_once("/var/www/html/api/database/exec/execSelect.php");
 require_once("/var/www/html/api/database/exec/execInsert.php");
 require_once("/var/www/html/api/database/exec/execDelete.php");
 require_once("/var/www/html/api/database/exec/execUpdate.php");
+require_once("/var/www/html/api/database/exec/execverifyRegister.php");
 
 class QueryExec extends Database{
 
@@ -14,12 +15,15 @@ class QueryExec extends Database{
     private $insert;
     private $delete;
     private $update;
+    private $register;
 
     public function __construct(){
         $this->select = new SelectDatabase();
         $this->insert = new InsertDatabase();
         $this->delete = new DeleteDatabase();
         $this->update = new UpdateDatabase();
+        $this->register = new VerifyRegisterDatabase();
+
     }
 
     public function execQuery(string $query, string $param){
@@ -29,15 +33,19 @@ class QueryExec extends Database{
         }
         
         if($param == "select"){
-            return $this->select->selectExec($this->mysql, $query);
+            return $this->select->selectExec($query);
         }
 
         if($param == "delete"){
-            return $this->delete->deleteExec($this->mysql, $query);                
+            return $this->delete->deleteExec($query);                
         }
 
         if($param == "update"){
-            return $this->update->updateExec($this->mysql, $query);
+            return $this->update->updateExec($query);
+        }
+
+        if($param == "register"){
+            return $this->register->registerExec($query);
         }
     }
 }
