c754950806ae3eb847d3e0555854bd54296723e3
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 19:37:46 -0300

    Update controllerGet.php

--- src/api/controller/controllerGet.php
+++ src/api/controller/controllerGet.php
@@ -5,10 +5,28 @@ require_once("../model/cadastroModelGet.php");
 class ControllerGet extends CadastroModelGet{
 
     public function getId($param){
-        return $this->modelGetId($param);
+        
+        $ret = $this->modelGetId($param);
+
+        if(count($ret)>0){
+            http_response_code(302);
+            return $ret;
+        }
+        http_response_code(404);
+        $arr = array("Message: "=>"Registro nao encontrado.");
+        return $arr;
     }
 
     public function getAll(){
-        return $this->modelGetAll();
+       
+        $ret = $this->modelGetAll();
+        
+        if(count($ret)>0){
+            http_response_code(302);
+            return $ret;
+        }
+        http_response_code(404);
+        $arr = array("Message: "=>"Nenhum registro encontrado.");
+        return $arr;
     }
 }
