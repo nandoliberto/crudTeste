546654f463dbb731949d4b3c8d113ad33b57e887
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 20:55:14 -0300

    Update get.php

--- src/api/handler/get.php
+++ src/api/handler/get.php
@@ -6,8 +6,8 @@ class Get extends ControllerGet{
 
     public function getCadastro($param){
         
-       if(isset($param[0]) && $param[0] != ""){
-            return $this->getId($param);
+        if($param[0] == "produtos" && is_numeric($param[1])){
+            return $this->getId($param[1]);
         }
 
         return $this->getAll();
