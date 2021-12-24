f6252316c9eba91cb9645b88577903cb1a30114a
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 22:42:41 -0300

    Add files via upload

--- src/api/controller/controllerPost.php
+++ src/api/controller/controllerPost.php
@@ -1,5 +1,5 @@
 <?php
-include_once(dirname(__DIR__)."/model/cadastroModelPost.php");
+require_once(dirname(__DIR__)."/model/cadastroModelPost.php");
 require_once("valDataPost/valDataPost.php");
 
 class ControllerPost extends CadastroModel{
