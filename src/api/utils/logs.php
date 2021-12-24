a8716e94ddffcfd2ad1a3c4358d2d1764c5548b7
nandoliberto <48319677+nandoliberto@users.noreply.github.com>
2021-12-23 23:19:16 -0300

    Update logs.php

--- src/api/utils/logs.php
+++ src/api/utils/logs.php
@@ -4,6 +4,11 @@ class Logs{
 
     public function writeLog($msg){
         try {
+            
+            if(!is_dir("/var/www/html/Logs/")){
+                mkdir("/var/www/html/Logs/");
+            }
+
             file_put_contents("/var/www/html/Logs/Logs.txt", $msg.";".PHP_EOL, FILE_APPEND);
         } catch (\Exeception $e) {
             throw(json_encode("Erro :".__FUNCTION__." ". $e->getMessage()));
